90934f283d3de5726ca9a43675ecf81cc7e8bc71
Nancy <68706811+nancy-dassana@users.noreply.github.com>
2020-09-03 17:33:52 -0700

    v0.2.0 -> v0.3.0 (#17)
    * feat #15 - Add util to emit notification events (#16)

--- src/__tests__/eventUtils.test.ts
+++ src/__tests__/eventUtils.test.ts
@@ -1,12 +1,34 @@
-import { dassanaEmitter } from '../eventUtils'
 import mitt from 'mitt'
+import {
+	dassanaEmitter,
+	DassanaEventTypes,
+	emitNotificationEvent
+} from '../eventUtils'
 
 jest.mock('mitt')
+
+const mockEmitter = {
+	emit: jest.fn()
+}
 
 describe('dassanaEmitter', () => {
 	it('should return an instance of an event emitter', () => {
 		dassanaEmitter
 
 		expect(mitt).toHaveBeenCalled()
+	})
+})
+
+describe('emitNotificationEvent', () => {
+	const mockMessage = 'foo'
+
+	emitNotificationEvent(
+		DassanaEventTypes.error,
+		mockMessage,
+		mockEmitter as any
+	)
+
+	expect(mockEmitter.emit).toHaveBeenCalledWith(DassanaEventTypes.error, {
+		message: mockMessage
 	})
 })
