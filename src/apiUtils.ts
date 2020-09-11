2023bd9f9e1d64c16cad2f338cb9ab7f6be8e934
Nancy <68706811+nancy-dassana@users.noreply.github.com>
2020-09-10 17:30:36 -0700

    v0.3.3 -> v0.3.4 (#35)
    * feat #32 Update handleAjaxErrors (#33)
    
    * feat #34 - Update api function to take in baseUrl

--- src/apiUtils.ts
+++ src/apiUtils.ts
@@ -1,30 +1,46 @@
 import axiosRetry from 'axios-retry'
-import { ErrorInfo } from 'api'
 import jsonmergepatch from 'json-merge-patch'
 import omit from 'lodash/omit'
 import { v4 as uuidv4 } from 'uuid'
-import axios, { AxiosInstance } from 'axios'
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig } from 'axios'
 import { Emitter, ev } from './eventUtils'
+import { ErrorInfo, InternalError } from 'api'
 
 export const DASSANA_REQUEST_ID = 'x-dassana-request-id'
 export const TOKEN = 'token'
 
-export const api: () => AxiosInstance = () => {
-	const apiClient = axios.create({
+export type DassanaError = ErrorInfo | InternalError
+export type DassanaAxiosInstance = AxiosInstance
+
+export const api: (apiUrl?: string) => AxiosInstance = (apiUrl = '') => {
+	const axiosRequestConfig: AxiosRequestConfig = {
 		headers: {
 			Authorization: `Bearer ${localStorage.getItem(TOKEN)}`,
 			[DASSANA_REQUEST_ID]: uuidv4()
 		}
-	})
+	}
+
+	if (apiUrl) {
+		axiosRequestConfig.baseURL = apiUrl
+	}
+
+	const apiClient = axios.create(axiosRequestConfig)
+
 	axiosRetry(apiClient)
 
 	return apiClient
 }
 
 export const handleAjaxErrors = (
-	{ key, msg }: ErrorInfo,
+	{ response }: AxiosError<DassanaError>,
 	emitter: Emitter
-): void => emitter.emitNotificationEvent(ev.error, msg ? msg : key)
+): void => {
+	if (response) {
+		const { key, msg } = response.data
+
+		return emitter.emitNotificationEvent(ev.error, msg ? msg : key)
+	}
+}
 
 interface PatchInfo<T, U> {
 	fieldValues: U
