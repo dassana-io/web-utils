e3c39b6d242832c34effa6211c1ea4f425307b40
Sam M <68707128+sam-m-m@users.noreply.github.com>
2021-02-17 23:19:27 -0800

    v0.7.8 -> v0.7.9 (#80)

--- src/hookUtils.ts
+++ src/hookUtils.ts
@@ -17,36 +17,95 @@ export const usePrevious = <T>(state: T): T | undefined => {
 	return ref.current
 }
 
+// -----------------------------------
 type KeyboardEventTypes = 'keydown' | 'keyup'
 
-export interface UseShortcutConfig {
+interface CommonUseShorcutConfig {
 	additionalConditionalFn?: () => boolean
 	callback: () => void
+	preventDefault?: boolean
+}
+
+export interface MultipleKeysUseShorcut extends CommonUseShorcutConfig {
+	// For now keys can only inlcude two keys. If adding more than two keys, read this first https://stackoverflow.com/questions/27380018/when-cmd-key-is-kept-pressed-keyup-is-not-triggered-for-any-other-key/27512489#27512489
+	keys: [KeyboardEvent['key'], KeyboardEvent['key']]
+}
+
+export interface SingleKeyUseShorcut extends CommonUseShorcutConfig {
 	key: KeyboardEvent['key']
 	keyEvent: KeyboardEventTypes
 }
 
+export type UseShortcutConfig = SingleKeyUseShorcut | MultipleKeysUseShorcut
+
 export const useShortcut = ({
 	additionalConditionalFn = () => true,
 	callback,
-	key,
-	keyEvent
+	preventDefault = false,
+	...rest
 }: UseShortcutConfig) => {
-	const onKeyEvent = useCallback(
-		(event: KeyboardEvent) => {
-			if (event.key === key && additionalConditionalFn()) {
-				callback()
-			}
-		},
-		[additionalConditionalFn, callback, key]
-	)
+	const [isFirstKeyPressed, setIsFirstKeyPressed] = useState(false)
 
 	useEffect(() => {
-		window.addEventListener(keyEvent, onKeyEvent)
+		// If shortcut uses single key, a keyEvent is provided - use onKeyEvent and attach it to window
+		if ('key' in rest) {
+			const { key, keyEvent } = rest
 
-		return () => window.removeEventListener(keyEvent, onKeyEvent)
-	}, [keyEvent, onKeyEvent])
+			const onKeyEvent = (event: KeyboardEvent) => {
+				// This prevents callback from being called if a key is pressed continuosly without lifting finger
+				if (event.repeat) return
+
+				if (event.key === key && additionalConditionalFn()) {
+					callback()
+
+					if (preventDefault) event.preventDefault()
+				}
+			}
+
+			window.addEventListener(keyEvent, onKeyEvent)
+
+			return () => window.removeEventListener(keyEvent, onKeyEvent)
+			// Otherwise shortcut uses multiple keys. We need to use both onKeyDown and onKeyUp to know which keys have been pressed
+		} else {
+			const [firstKey, secondKey] = rest.keys
+
+			const onKeyDown = (event: KeyboardEvent) => {
+				if (event.repeat) return
+
+				if (event.key === firstKey) setIsFirstKeyPressed(true)
+				else if (
+					isFirstKeyPressed &&
+					event.key === secondKey &&
+					additionalConditionalFn()
+				) {
+					callback()
+
+					if (preventDefault) event.preventDefault()
+				}
+			}
+
+			const onKeyUp = (event: KeyboardEvent) => {
+				if (firstKey === event.key) setIsFirstKeyPressed(false)
+			}
+
+			window.addEventListener('keydown', onKeyDown)
+			window.addEventListener('keyup', onKeyUp)
+
+			return () => {
+				window.removeEventListener('keydown', onKeyDown)
+				window.removeEventListener('keyup', onKeyUp)
+			}
+		}
+	}, [
+		additionalConditionalFn,
+		callback,
+		isFirstKeyPressed,
+		preventDefault,
+		rest
+	])
 }
+
+// -----------------------------------
 
 export enum ThemeType {
 	dark = 'dark',
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+// Onboarding step number enum used in web-orchestrator and web-onboarding
+export enum OnboardingStepNum {
+	userName = 1,
+	userWorkHours = 2,
+	userPersona = 3,
+	orgName = 4
+}
