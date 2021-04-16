225b34e54b191e8d1096ebdce68d6430ef413d4b
Sam M <68707128+sam-m-m@users.noreply.github.com>
2021-04-16 11:21:00 -0700

    chore #89 - chore - move time util function from web-alert-manager (#90)

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,10 @@
+/**
+ * More info on Distributive Conditional Types: https://stackoverflow.com/a/57103940/11279811
+ */
+export type DistributiveOmit<T, K extends keyof T> = T extends T
+	? Omit<T, K>
+	: never
+
 // Onboarding step number enum used in web-orchestrator and web-onboarding
 export enum OnboardingStepNum {
 	userName = 1,
