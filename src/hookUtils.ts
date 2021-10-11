d9d07e6618aed05ca252501f4171f7c9e17f644a
Nancy <68706811+nancy-dassana@users.noreply.github.com>
2021-10-11 12:43:13 -0700

    v0.8.9 -> v0.8.10
    chore #115 - Extract out useInView to use across repos (#116)
    
    Closes #115

--- src/hookUtils.ts
+++ src/hookUtils.ts
@@ -9,7 +9,9 @@ import {
 	useState
 } from 'react'
 
-interface UseClickOutseConfig {
+// -----------------------------------
+
+interface UseClickOutsideConfig {
 	callback: (key?: string) => void
 	/* The keys which will trigger the callback. Defaults to ['Escape'] */
 	keys?: string[]
@@ -18,7 +20,7 @@ interface UseClickOutseConfig {
 export const useClickOutside = ({
 	callback,
 	keys = ['Escape']
-}: UseClickOutseConfig) => {
+}: UseClickOutsideConfig) => {
 	const ref = useRef(null)
 
 	useEffect(() => {
@@ -45,6 +47,73 @@ export const useClickOutside = ({
 
 // -----------------------------------
 
+/**
+ * Custom hook that informs when a div element is in view of the
+ * browser viewport.
+ */
+export const useInView = () => {
+	const ref = useRef<HTMLDivElement>(null)
+
+	const [inView, setInView] = useState(false)
+
+	/**
+	 * This useEffect gets called on mount and creates an instance of
+	 * IntersectionObserver. It keeps track of when an element enters
+	 * and leaves the browser viewport.
+	 */
+	useEffect(() => {
+		const refCurr = ref.current
+
+		if (refCurr) {
+			/**
+			 * Callback passed into the IntersectionObserver() constructor.
+			 * Since we're only observing one div element, check if only
+			 * the first item - entries[0] is in view.
+			 */
+			const callback = ([entry]: IntersectionObserverEntry[]) =>
+				setInView(entry.isIntersecting)
+
+			/**
+			 * The options object passed into the IntersectionObserver() constructor.
+			 */
+			const options = {
+				/**
+				 * The element that is used as the viewport for checking
+				 * visibility of the target. Defaults to the browser viewport
+				 * if not specified or if null.
+				 */
+				root: null,
+				/**
+				 * Either a single number or an array of numbers which indicate at
+				 * what percentage of the target's visibility the observer's callback
+				 * should be executed
+				 */
+				threshold: 0
+			}
+
+			const observer = new IntersectionObserver(callback, options)
+
+			/**
+			 * Observer will observe element passed as params and
+			 * fire callback when it enters or leaves viewframe.
+			 */
+			observer.observe(refCurr)
+
+			/**
+			 * Cleanup by unobserving the element and disconnecting observer.
+			 */
+			return () => {
+				observer.unobserve(refCurr)
+				observer.disconnect()
+			}
+		}
+	}, [])
+
+	return { inView, ref }
+}
+
+// -----------------------------------
+
 export const usePrevious = <T>(state: T): T | undefined => {
 	const ref = useRef<T>()
 
@@ -56,6 +125,7 @@ export const usePrevious = <T>(state: T): T | undefined => {
 }
 
 // -----------------------------------
+
 type KeyboardEventTypes = 'keydown' | 'keyup'
 
 interface CommonUseShorcutConfig {
@@ -65,7 +135,7 @@ interface CommonUseShorcutConfig {
 }
 
 export interface MultipleKeysUseShorcut extends CommonUseShorcutConfig {
-	// For now keys can only inlcude two keys. If adding more than two keys, read this first https://stackoverflow.com/questions/27380018/when-cmd-key-is-kept-pressed-keyup-is-not-triggered-for-any-other-key/27512489#27512489
+	// For now keys can only include two keys. If adding more than two keys, read this first https://stackoverflow.com/questions/27380018/when-cmd-key-is-kept-pressed-keyup-is-not-triggered-for-any-other-key/27512489#27512489
 	keys: [KeyboardEvent['key'], KeyboardEvent['key']]
 }
 
@@ -90,7 +160,7 @@ export const useShortcut = ({
 			const { key, keyEvent } = rest
 
 			const onKeyEvent = (event: KeyboardEvent) => {
-				// This prevents callback from being called if a key is pressed continuosly without lifting finger
+				// This prevents callback from being called if a key is pressed continuously without lifting finger
 				if (event.repeat) return
 
 				if (event.key === key && additionalConditionalFn()) {
