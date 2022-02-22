506c95ee11492c9e0a168496065a1054eadac728
Nancy <68706811+nancy-dassana@users.noreply.github.com>
2022-02-22 15:45:27 -0800

    chore #147 - Use date-fns instead of moment (#148)

--- src/__tests__/timeUtils.tests.ts
+++ src/__tests__/timeUtils.tests.ts
@@ -3,29 +3,25 @@ import {
 	convertEpochToUserTimezone
 } from '../timeUtils'
 
-jest.mock('moment-timezone', () => {
-	const moment = jest.requireActual('moment-timezone')
-	// To prevent tests failing for future remote teams
-	moment.tz.setDefault('America/Los_Angeles')
-	moment.tz.guess = () => 'America/Los_Angeles'
-	moment.now = () => new Date('Wed, Nov 14, 2020 11:02 AM PDT')
-
-	return moment
-})
+Date.now = jest.fn(() => 1605380520000)
 
 describe('convertEpochToRelativeTime', () => {
 	it('should convert epoch to relative time', () => {
 		const time = convertEpochToRelativeTime(1602698523200)
 
-		expect(time).toMatch('a month ago')
+		expect(time).toMatch('1 month ago')
 	})
 })
 
 describe('convertEpochToUserTimezone', () => {
 	it('should convert epoch to a human friendly date in the correct timezone', () => {
-		const time = convertEpochToUserTimezone(1602698523200)
+		const time = convertEpochToUserTimezone(
+			1602698523200,
+			undefined,
+			'America/Los_Angeles'
+		)
 
-		expect(time).toMatch('Wed, Oct 14, 2020 11:02 AM PDT')
+		expect(time).toMatch('Wed, Oct 14 2020 11:02 AM PDT')
 	})
 
 	it('should return an empty string if no time is passed in', () => {
