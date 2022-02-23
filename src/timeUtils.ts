4d444f1777ffa1c189b2d61f69f388ee2acec050
Jasper <97872491+jasper-dassana@users.noreply.github.com>
2022-02-23 16:45:56 -0600

    Add util fn for guessing user's timezone offset

--- src/timeUtils.ts
+++ src/timeUtils.ts
@@ -9,3 +9,13 @@ export const convertEpochToUserTimezone = (
 	timeFormat = 'EEE, MMM d yyyy hh:mm a z',
 	timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
 ): string => (time ? format(time, timeFormat, { timeZone }) : '')
+
+/**
+ * The `getTimezoneOffset()` method returns the difference, in minutes,
+ * between a date as evaluated in the UTC time zone, and the same date
+ * as evaluated in the local time zone.
+ *
+ *     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getTimezoneOffset
+ */
+export const guessUserTimezoneOffset = () =>
+	-(new Date().getTimezoneOffset() / 60)
