e410a1124ee7f94473025678cdfd53422552455e
Nancy Lai <nancy@dassana.io>
2022-04-09 10:33:01 -0700

    feat #163 - useDebounce hook

--- src/__tests__/hookUtils/index.test.ts
+++ src/__tests__/hookUtils/index.test.ts
@@ -3,6 +3,7 @@ import { act, renderHook } from '@testing-library/react-hooks'
 import { Emitter, EmitterEventTypes } from '../../eventUtils'
 import {
 	ThemeType,
+	useDebounce,
 	usePrevious,
 	useQueryParams,
 	useTheme
@@ -13,6 +14,55 @@ const mockBrowserLocation = {
 }
 
 initializeLocalStorageMock()
+
+const mockSetTimeout = () => {
+	jest.useFakeTimers()
+	jest.spyOn(global, 'setTimeout')
+}
+
+const mockClearTimeout = () => {
+	jest.useFakeTimers()
+	jest.spyOn(global, 'clearTimeout')
+}
+
+describe('useDebounce', () => {
+	afterEach(() => {
+		jest.useRealTimers()
+	})
+
+	test('should return debounce value', () => {
+		const value = 'value'
+		const {
+			result: { current: debounceValue }
+		} = renderHook(() => useDebounce(value))
+
+		expect(value).toBe(debounceValue)
+	})
+
+	test('should debounce with default debounce 250 ms', () => {
+		mockSetTimeout()
+		renderHook(() => useDebounce('value'))
+
+		expect(setTimeout).toHaveBeenCalledTimes(1)
+		expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 250)
+	})
+
+	test('should debounce with given debounce', () => {
+		mockSetTimeout()
+		renderHook(() => useDebounce('value', 1337))
+
+		expect(setTimeout).toHaveBeenCalledTimes(1)
+		expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1337)
+	})
+
+	test('should call clearTimeout on unmount', () => {
+		mockClearTimeout()
+		const { unmount } = renderHook(() => useDebounce('value'))
+		unmount()
+
+		expect(clearTimeout).toHaveBeenCalledTimes(1)
+	})
+})
 
 describe('usePrevious', () => {
 	const initialProps = { state: 'foo' }
