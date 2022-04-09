ee740ee2e2f1a7f053068603d0d0d184e79558e6
Nancy <68706811+nancy-dassana@users.noreply.github.com>
2022-04-09 10:37:22 -0700

    feat #163 - useDebounce hook (#164)

--- src/hookUtils.ts
+++ src/hookUtils.ts
@@ -52,6 +52,27 @@ export const useClickOutside = ({
 
 // -----------------------------------
 
+export const useDebounce = <T>(value: T, delay = 250) => {
+	const [debouncedValue, setDebouncedValue] = useState(value)
+
+	useEffect(
+		() => {
+			// Update debounced value after delay
+			const handler = setTimeout(() => setDebouncedValue(value), delay)
+
+			// Cancel the timeout if value changes (also on delay change or unmount)
+			// This is how we prevent debounced value from updating if value is changed
+			// within the delay period. Timeout gets cleared and restarted.
+			return () => clearTimeout(handler)
+		},
+		[delay, value] // Only re-call effect if value or delay changes
+	)
+
+	return debouncedValue
+}
+
+// -----------------------------------
+
 /**
  * Custom hook that tracks an element's height and width
  * Dimension outputs update on resize via observer
