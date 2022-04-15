d7c373f15f0cd9d4220a25416c99853bd2bf1048
Jasper <97872491+jasper-dassana@users.noreply.github.com>
2022-04-14 17:12:13 -0700

    Refactor timezone utils

--- src/timeUtils.ts
+++ src/timeUtils.ts
@@ -1,5 +1,8 @@
 import { format } from 'date-fns-tz'
 import { formatDistanceToNowStrict } from 'date-fns'
+import he from 'he'
+import sortBy from 'lodash/sortBy'
+import { TimezoneV2 } from 'api/profile'
 
 export const convertEpochToRelativeTime = (time: number) =>
 	formatDistanceToNowStrict(time, { addSuffix: true })
@@ -9,6 +12,9 @@ export const convertEpochToUserTimezone = (
 	timeFormat = 'EEE, MMM d yyyy hh:mm a z',
 	timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
 ): string => (time ? format(time, timeFormat, { timeZone }) : '')
+
+export const filterOutExtraTimezones = ({ id }: TimezoneV2) =>
+	id && !id.startsWith('Etc/')
 
 /**
  * The `getTimezoneOffset()` method returns the difference, in minutes,
@@ -22,3 +28,36 @@ export const guessUserTimezoneOffset = () =>
 
 export const guessUserTimezone = (): string =>
 	Intl.DateTimeFormat().resolvedOptions().timeZone
+
+export interface TimezoneOption {
+	offset: number
+	text: string
+	value: string
+}
+
+export const mapTimezoneOptions = ({ id = '', utcOffset }: TimezoneV2) => {
+	const offset = !utcOffset || utcOffset === 'Z' ? '±00:00' : utcOffset
+
+	const parseOffset = (offset: string): number => {
+		const value = offset.replace('±', '').replace(':', '.')
+
+		return parseFloat(value)
+	}
+
+	const display = `(UTC${offset}) ${id}`
+
+	return {
+		offset: parseOffset(offset),
+		text: he.decode(display),
+		value: id
+	}
+}
+
+export const sortTimezones = (
+	timezones: TimezoneV2[],
+	factor: string
+): TimezoneOption[] =>
+	sortBy(
+		timezones.filter(filterOutExtraTimezones).map(mapTimezoneOptions),
+		factor
+	)
