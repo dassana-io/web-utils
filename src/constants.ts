b5c024bee7bf1f136fd44d73d53c7faab0d0bb81
Jasper <97872491+jasper-dassana@users.noreply.github.com>
2022-04-20 13:05:43 -0700

    Add and export email regex

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,8 @@
 import { KeyMapConfig, ModifierKeys, OperatingSystems } from 'types'
 
+export const EMAIL_REGEX =
+	/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
 export const MICRO_FE_CONTAINER_ID = 'micro-fe-container'
 
 export const ONBOARDING_DEFAULT_STEP = 'onboardingDefaultStep'
